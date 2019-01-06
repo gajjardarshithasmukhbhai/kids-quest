f40b75de123db7ed45cd6ef826c6f3735eb31208
gajjardarshithasmukhbhai <darshit.gajjar1998@gmail.com>
2019-01-06 14:33:41 +0530

    dmdmmd
    mm

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const app=express();
 app.get("/darshit/api",(req,res,next)=>{
-	const customer=[{id:1,firstname:'hasmukhBhai'},{id:2,firstname:'snehaben'},{id:3,firstname:'rathodbhai'}];
+	const customer=[{id:1,firstname:'hasmukBhai'},{id:2,firstname:'snehaben'},{id:3,firstname:'rathodbhai'}];
 	res.json(customer);
 });
 app.get("/hasmukh/api",(req,res,next)=>{
